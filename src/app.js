e9d09b1356a35fd59a8176eb326bdcef94956212
MED LAQ <medlaq@titanos.io>
2025-11-21 14:00:00 +0100

    feat: install cors, helmet, morgan and wire security middleware [TIT-43]

--- src/app.js
+++ src/app.js
@@ -1,7 +1,24 @@
+import "dotenv/config";
 import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
 
 const app = express();
 
+const corsOptions = {
+  origin: process.env.CORS_ORIGIN?.split(",") ?? ["http://localhost:3000"],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
+
+app.use(helmet());
+app.use(cors(corsOptions));
+app.options("/api/*", cors(corsOptions));
+app.use(morgan("dev"));
+app.disable("x-powered-by");
+app.use("/api/webhook", express.raw({ type: "application/json" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
