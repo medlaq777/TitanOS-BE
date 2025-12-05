51d30b50364fddd2fbd1487332247fa0695ef5b5
MED LAQ <medlaq@titanos.io>
2025-12-05 15:00:00 +0100

    feat: restrict access to sensitive medical module [TIT-85]
    - Apply authGuard + rolesGuard("ADMIN", "STAFF") to all medical routes
    - Prevents PLAYER and FAN roles from accessing medical records
    
    [TIT-85]

--- src/routers/medical.routes.js
+++ src/routers/medical.routes.js
@@ -1,5 +1,10 @@
 import { Router } from "express";
+import { authGuard } from "../middlewares/authGuard.js";
+import { rolesGuard } from "../middlewares/rolesGuard.js";
 
 const medicalRouter = Router();
+
+medicalRouter.use(authGuard);
+medicalRouter.use(rolesGuard("ADMIN", "STAFF"));
 
 export default medicalRouter;
