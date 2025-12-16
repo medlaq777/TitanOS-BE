4bf30bd57abc99c3fa47ad0858f7f1f6e86fdb50
MED LAQ <med.laq@titanos.dev>
2025-12-16 14:00:00 +0100

    feat: store MinIO file references in medical repository [TIT-108]
    - addFileReference(): push new URL into fileUrls array
    - refine member select fields in findAll and findById
    - createRecord includes member info in response

--- src/repositories/medical.repository.js
+++ src/repositories/medical.repository.js
@@ -6,21 +6,34 @@ export class MedicalRepository {
   findAllRecords(memberId) {
     return this.prisma.medicalRecord.findMany({
       where: memberId ? { memberId } : undefined,
-      include: { member: true },
+      include: { member: { select: { id: true, firstName: true, lastName: true } } },
       orderBy: { recordedAt: 'desc' },
     });
   }
 
   findRecordById(id) {
-    return this.prisma.medicalRecord.findUnique({ where: { id }, include: { member: true } });
+    return this.prisma.medicalRecord.findUnique({
+      where: { id },
+      include: { member: { select: { id: true, firstName: true, lastName: true } } },
+    });
   }
 
   createRecord(data) {
-    return this.prisma.medicalRecord.create({ data, include: { member: true } });
+    return this.prisma.medicalRecord.create({
+      data,
+      include: { member: { select: { id: true, firstName: true, lastName: true } } },
+    });
   }
 
   updateRecord(id, data) {
     return this.prisma.medicalRecord.update({ where: { id }, data });
+  }
+
+  addFileReference(id, fileUrl) {
+    return this.prisma.medicalRecord.update({
+      where: { id },
+      data: { fileUrls: { push: fileUrl } },
+    });
   }
 
   deleteRecord(id) {
